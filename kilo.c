97e164f462f1dbbf9f2d4393abd1f017596e54fa
bloorq@gmail.com <bloorq@gmail.com>
2017-07-25 09:04:24 +0100

    fcode reformating

--- kilo.c
+++ kilo.c
@@ -24,7 +24,6 @@ int main()
     enableRawMode();
 
     char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
-        ;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
     return 0;
 }
