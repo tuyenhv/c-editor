a7dbd8b76ea7e41090f5617121ebb10f2e10c995
bloorq@gmail.com <bloorq@gmail.com>
2017-07-26 20:03:57 +0100

    Display keypresses

--- kilo.c
+++ kilo.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -24,6 +26,16 @@ int main()
     enableRawMode();
 
     char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+    {
+        if (iscntrl(c))
+        {
+            printf("%d\n", c);
+        }
+        else
+        {
+            printf("%d ('%c')\n", c, c);
+        }
+    }
     return 0;
 }
