c8dd4b6c030d1cb618ecc82cb19a50fa01e73758
bloorq@gmail.com <bloorq@gmail.com>
2017-08-04 00:18:27 +0100

    Welcome message - step 42

--- kilo.c
+++ kilo.c
@@ -135,6 +135,13 @@ void editorDrawRows(struct abuf *ab) {
                                 "Kilo editor -- version %s", KILO_VERSION);
       if (welcomelen > E.screencols)
         welcomelen = E.screencols;
+      int padding = (E.screencols - welcomelen) / 2;
+      if (padding) {
+        abAppend(ab, "~", 1);
+        padding--;
+      }
+      while (padding--)
+        abAppend(ab, " ", 1);
       abAppend(ab, welcome, welcomelen);
     } else {
       abAppend(ab, "~", 1);
