8e0ca8ebd5016490ff3c9eb438c3a5db09ed1227
bloorq@gmail.com <bloorq@gmail.com>
2017-08-29 23:05:13 +0100

    Inserting characters - Step 103

--- kilo.c
+++ kilo.c
@@ -262,6 +262,16 @@ void editorRowInsertChar(erow *row, int at, int c) {
   editorUpdateRow(row);
 }
 
+/** editor operations **/
+
+void editorInsertChar(int c) {
+  if (E.cy == E.numrows) {
+    editorAppendRow("", 0);
+  }
+  editorRowInsertChar(&E.row[E.cy], E.cx, c);
+  E.cx++;
+}
+
 /** file i/o **/
 
 void editorOpen(char *filename) {
@@ -505,6 +515,10 @@ void editorProcessKeypress() {
   case ARROW_LEFT:
   case ARROW_RIGHT:
     editorMoveCursor(c);
+    break;
+
+  default:
+    editorInsertChar(c);
     break;
   }
 }
