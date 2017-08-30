e941651803c6fdfa1e260af41a03c144df194aed
bloorq@gmail.com <bloorq@gmail.com>
2017-08-30 22:39:05 +0100

    Handling special keys - Step 104

--- kilo.c
+++ kilo.c
@@ -22,6 +22,7 @@
 #define CTRL_KEY(k) ((k)&0x1f)
 
 enum editorKey {
+  BACKSPACE = 127,
   ARROW_LEFT = 1000,
   ARROW_RIGHT,
   ARROW_UP,
@@ -481,6 +482,9 @@ void editorProcessKeypress() {
   int c = editorReadKey();
 
   switch (c) {
+  case '\r':
+    /*TODO*/
+    break;
   case CTRL_KEY('q'):
     write(STDOUT_FILENO, "\x1b[2J", 4);
     write(STDOUT_FILENO, "\x1b[H", 3);
@@ -490,9 +494,16 @@ void editorProcessKeypress() {
   case HOME_KEY:
     E.cx = 0;
     break;
+
   case END_KEY:
     if (E.cy < E.numrows)
       E.cx = E.row[E.cy].size;
+    break;
+
+  case BACKSPACE:
+  case CTRL_KEY('h'):
+  case DEL_KEY:
+    /*TODO*/
     break;
 
   case PAGE_UP:
@@ -515,6 +526,10 @@ void editorProcessKeypress() {
   case ARROW_LEFT:
   case ARROW_RIGHT:
     editorMoveCursor(c);
+    break;
+
+  case CTRL_KEY('l'):
+  case '\x1b':
     break;
 
   default:
