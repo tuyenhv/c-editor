dd1943011948dd08c856b7321977ca085d3d7237
bloorq@gmail.com <bloorq@gmail.com>
2017-09-10 23:14:01 +0100

    Prevent quitting when unsaved changes - Step 115

--- kilo.c
+++ kilo.c
@@ -19,6 +19,7 @@
 /** defines **/
 #define KILO_VERSION "0.0.1"
 #define KILO_TAB_STOP 8
+#define KILO_QUIT_TIMES 3
 
 #define CTRL_KEY(k) ((k)&0x1f)
 
@@ -261,7 +262,7 @@ void editorAppendRow(char *s, size_t len) {
 
 void editorRowInsertChar(erow *row, int at, int c) {
   if (at < 0 || at > row->size)
-  at = row->size;
+    at = row->size;
   row->chars = realloc(row->chars, row->size + 2);
   memmove(&row->chars[at + 1], &row->chars[at], row->size - at + 1);
   row->size++;
@@ -531,6 +532,8 @@ void editorMoveCursor(int key) {
 }
 
 void editorProcessKeypress() {
+  static int quit_times = KILO_QUIT_TIMES;
+
   int c = editorReadKey();
 
   switch (c) {
@@ -538,6 +541,13 @@ void editorProcessKeypress() {
     /*TODO*/
     break;
   case CTRL_KEY('q'):
+    if (E.dirty && quit_times > 0) {
+      editorSetStatusMessage("WARNING!!! File has unsaved changes. "
+                             "Press Ctrl-Q %d more times to quit.",
+                             quit_times);
+      quit_times--;
+      return;
+    }
     write(STDOUT_FILENO, "\x1b[2J", 4);
     write(STDOUT_FILENO, "\x1b[H", 3);
     exit(0);
@@ -592,6 +602,8 @@ void editorProcessKeypress() {
     editorInsertChar(c);
     break;
   }
+
+  quit_times = KILO_QUIT_TIMES;
 }
 
 /*** init ***/
