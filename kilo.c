343900e6a37f0ef202fbb175b2d932c4c36ec155
bloorq@gmail.com <bloorq@gmail.com>
2017-09-11 23:26:57 +0100

    Inserting new line - Step 125

--- kilo.c
+++ kilo.c
@@ -315,6 +315,21 @@ void editorInsertChar(int c) {
   E.cx++;
 }
 
+void editorInsertNewLine() {
+  if (E.cx == 0) {
+    editorInsertRow(E.cy, "", 0);
+  } else {
+    erow *row = &E.row[E.cy];
+    editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+    row = &E.row[E.cy];
+    row->size = E.cx;
+    row->chars[row->size] = '\0';
+    editorUpdateRow(row);
+  }
+  E.cy++;
+  E.cx = 0;
+}
+
 void editorDelChar() {
   if (E.cy == E.numrows)
     return;
@@ -591,7 +606,7 @@ void editorProcessKeypress() {
 
   switch (c) {
   case '\r':
-    /*TODO*/
+    editorInsertNewLine();
     break;
   case CTRL_KEY('q'):
     if (E.dirty && quit_times > 0) {
